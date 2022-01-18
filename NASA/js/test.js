180da6b625ec37285087d4755bbc83471c5f01f3
CarterGillis1997 <92799737+CarterGillis1997@users.noreply.github.com>
2022-01-18 16:36:50 -0500

    Delete test.js

--- NASA/js/test.js
+++
@@ -1,9 +0,0 @@
-fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&api_key=DEMO_KEY")
-.then((resp) => resp.json())
-.then(function(data){
-    console.log(data)
-    for(let i = 0;i<data.photos.length;i++){
-    let img = $("<img>").attr("src",data.photos[i].img_src)
-    $("#main").append(img)
-    }
-})
